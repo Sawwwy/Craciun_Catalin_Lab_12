06e48f8fb2815cb2dbeec57b8e680b8226bb7648
user <user@DESKTOP-U4MVTGN>
2022-01-18 12:37:18 +0200

    Add project files.

---
+++ Craciun_Catalin_Lab12/Craciun_Catalin_Lab12/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Nume_Pren_Lab12.Data;
+namespace Craciun_Catalin_Lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
